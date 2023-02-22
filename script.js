948dd16dab9d59e8940513ef5dbfa6e84869e29f
Hector Cardenas <card.hectorl@gmail.com>
2023-02-22 17:38:00 -0600

    Added function that turns list into a string.

--- script.js
+++ script.js
@@ -42,22 +42,35 @@ class LinkedList {
     }
     contains(value){
         let i = 0;
-        while (i <= this.size()) {
+        while (i < this.size()) {
             let nodeValue = this.list[i].returnValue();
             if (value == nodeValue) {
                 return true;
             } if (this.size() == i && nodeValue != value) {
                 return false
             }
+            if (nodeValue == undefined) {
+                return console.log('does not exist');
+            }
             i++
         }
-        
+        return 'does not exist';  
     }
     find(value){
 
     }
-    toString(){
+    toString(listString = ''){
+        while (this.list.length >= 0) {
+            if (this.list.length < 1) {
+                return listString;
 
+            } else {
+                listString = `${listString} ${this.at(0).value}`;
+                this.list.shift();
+                
+            }
+
+        }
     }
 
 }
@@ -77,11 +90,17 @@ let newLinkedList = new LinkedList();
 newLinkedList.append('hex-gate');
 newLinkedList.append('hex-code');
 newLinkedList.prepend('sharigan-scrolls');
-newLinkedList.append('bubba-gump');
+newLinkedList.append('hexy');
+newLinkedList.append('juan');
+newLinkedList.append('luis');
 console.log(newLinkedList.list);
 console.log("HEAD");
 console.log(newLinkedList.head());
 console.log("TAIL");
 console.log(newLinkedList.tail());
 console.log(newLinkedList.contains('bubba-gump'));
+newLinkedList.pop()
+newLinkedList.pop()
+console.log(newLinkedList.toString())
+console.log(newLinkedList.list);
 
